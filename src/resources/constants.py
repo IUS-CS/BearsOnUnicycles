d8da86c07330566b56628b92a32b572c410bf58a
Jared Sanders <jtsanders95@gmail.com>
2018-02-27 20:35:09 -0500

    Add spritesheet and constant. Changing how sprites are done

---
+++ src/resources/constants.py
@@ -0,0 +1,16 @@
+"""
+Global Variables
+Initial commit: Jared Sanders
+
+Certain variables shouldn't be modified. These are denoted by
+the standard Python static syntax (i.e _varname_).
+
+Other values are free to change and have been named as just
+a regular variable.
+"""
+# Colors
+_BLACK_ = (0, 0, 0)
+
+#Screen Dimens
+_SCREEN_WIDTH_ = 800
+_SCREEN_HEIGHT_ = 600
