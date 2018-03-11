5b8165018d6775c48e56fa122152a66f6bf9502d
AlreadyAsleep <benheil@ius.edu>
2018-03-10 20:37:37 -0500

    Wrote game loop and input handler

---
+++ src/main.py
@@ -0,0 +1,44 @@
+# File: main.py
+# Authors: BearsOnUnicycles
+# Since: 3/10/18
+# This file calls all the main components of the game
+import os
+import pygame
+import input_handler as ih
+
+#=================================================================================
+# Static Variables section
+#
+#
+#=================================================================================
+
+SIZE = (900, 500)
+FPS = 15
+INPUT = ih.Handler()
+SCREEN = None
+
+
+# =================================================================================
+# Initialize pygame
+#
+# =================================================================================
+pygame.init()
+pygame.event.set_allowed([pygame.QUIT, pygame.KEYDOWN, pygame.KEYUP])
+path = os.path.dirname(os.path.realpath(__file__))
+SCREEN = pygame.display.set_mode(SIZE)
+
+
+# =================================================================================
+# Game Loop
+#
+# =================================================================================
+quitting = False
+while not quitting:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quitting = True
+    pygame.time.Clock().tick(FPS)
+
+
+
+
