eb5c46fdf8fc1d8d779603bdfd1f04ef44ed9edf
igillenh2o <igillenh2o@gmail.com>
2018-04-04 23:53:05 -0400

    input additions
    Added functionality for gamepad

--- src/input_handler.py
+++ src/input_handler.py
@@ -2,32 +2,89 @@
 # Authors: BearsOnUnicycles
 # Since: 3/10/18
 # this file handles all the input from the keyboard
-import pygame
+import pygame as pg
 
 
 class ButtonMap:
-    '''Struct that holds the button
-    mapping to pygame events'''
-    game_pad_present = False  #determines which button map to use
-    game_pads = []
-    map = {}
+    game_pad_present = False  # determines which map to use for input
+    game_pads = []  # holds configuration for each game pad/ joystick
 
-    default = {'P1_RIGHT': pygame.K_RIGHT,   \
-            'P1_LEFT': pygame.K_LEFT,    \
-            'P1_UP'  : pygame.K_UP,      \
-            'P1_DOWN': pygame.K_DOWN,    \
-     'P1_LIGHT_PUNCH': pygame.K_q,       \
-      'P1_MID_PUNCH' : pygame.K_w,       \
-     'P1_HEAVY_PUNCH': pygame.K_e,       \
-      'P1_LIGHT_KICK': pygame.K_a,       \
-        'P1_MID_KICK': pygame.K_s,       \
-      'P1_HEAVY_KICK': pygame.K_d,       \
-           'P1_PAUSE':pygame.K_ESCAPE    \
-    }
+    # holds the button mapping to pygame events regardless of whether via keyboard or game pad
+    input = {'P1_RIGHT',
+             'P1_LEFT',
+             'P1_UP',
+             'P1_DOWN',
+             'P1_LIGHT_PUNCH',
+             'P1_MID_PUNCH',
+             'P1_HEAVY_PUNCH',
+             'P1_LIGHT_KICK',
+             'P1_MID_KICK',
+             'P1_HEAVY_KICK',
+             'P1_PAUSE'
+             }
 
+#each pg.K_ value corrisponds to an index in the matrix returned by the function pg.key.get_pressed
+    keyboard_map = {'P1_RIGHT': pg.K_d,
+                    'P1_LEFT': pg.K_a,
+                    'P1_UP': pg.K_w,
+                    'P1_DOWN': pg.K_s,
+                    'P1_LIGHT_PUNCH': pg.K_u,
+                    'P1_MID_PUNCH': pg.K_i,
+                    'P1_HEAVY_PUNCH': pg.K_o,
+                    'P1_LIGHT_KICK': pg.K_j,
+                    'P1_MID_KICK': pg.K_k,
+                    'P1_HEAVY_KICK': pg.K_l,
+                    'P1_PAUSE': pg.K_t,
 
+                    'P2_RIGHT': pg.K_RIGHT,
+                    'P2_LEFT': pg.K_LEFT,
+                    'P2_UP': pg.K_UP,
+                    'P2_DOWN': pg.K_DOWN,
+                    'P2_LIGHT_PUNCH': pg.K_q,
+                    'P2_MID_PUNCH': pg.K_q,
+                    'P2_HEAVY_PUNCH': pg.K_q,
+                    'P2_LIGHT_KICK': pg.K_q,
+                    'P2_MID_KICK': pg.K_q,
+                    'P2_HEAVY_KICK': pg.K_q,
+                    'P2_PAUSE': pg.K_q,
 
+                    }
+    joystick_map_1_player = {'P1_RIGHT': 12,
+                    'P1_LEFT': 12,
+                    'P1_UP': 12,
+                    'P1_DOWN': 12,
+                    'P1_LIGHT_PUNCH': 4,
+                    'P1_MID_PUNCH': 0,
+                    'P1_HEAVY_PUNCH': 3,
+                    'P1_LIGHT_KICK': 6,
+                    'P1_MID_KICK': 1,
+                    'P1_HEAVY_KICK': 2,
+                    'P1_PAUSE': 9,
 
+                    'P2_RIGHT': 12,
+                    'P2_LEFT': 12,
+                    'P2_UP': 3,
+                    'P2_DOWN': 4,
+                    'P2_LIGHT_PUNCH': 5,
+                    'P2_MID_PUNCH': 6,
+                    'P2_HEAVY_PUNCH': 7,
+                     'P2_LIGHT_KICK': 8,
+                    'P2_MID_KICK': 9,
+                    'P2_HEAVY_KICK': 10,
+                    'P2_PAUSE': 11,
+                             }
+
+    def get_down_keyboard(self, key):
+        '''Using the pygame function to get input. All keys are returned by get_pressed.
+        We only want the ones we have mapped above so we feed in the index in question to get back what we want
+        '''
+
+        return pg.key.get_pressed()[key]
+
+    def get_down_gamepad(self, key):
+
+        print(self.game_pads[0].get_hat(0), ' get down gamepad function ran')
+        return self.game_pads[0].get_button(key)
 
     '''detect how many controllers are connected,
         determine which mapping to use,
@@ -35,124 +92,100 @@ class ButtonMap:
         display the name of that controller
     '''
     def set_gamepads(self):
-        pygame.joystick.init() #must be done before other joystick functions work
-        if pygame.joystick.get_count() > 0:
-            self.game_pad_present = True
-            count = pygame.joystick.get_count()
-            print(count, " game pads detected")
-            for i in range((count)):
+        pg.joystick.init()  # must be done before other joystick functions work
+        if pg.joystick.get_count() > 0:
+            pg.game_pad_present = True #todo fix it so that this takes place at the beginning but not every loop
+            joystick_count = pg.joystick.get_count()
+            print(joystick_count, "game pad(s) detected")
+            for i in range((joystick_count)):
             # add controller to joystick list
-                self.game_pads.append(pygame.joystick.Joystick(i))
-                self.game_pads[i].init() #must be initialized in the list
-                print('.Game pad detected: {} assigned to player {}'.format(self.game_pads[i].get_name(), (i+1) ))
-                i = i+1
-
-                self.map = {'P1_RIGHT': 5, \
-                           'P1_LEFT': 7, \
-                           'P1_UP': 4, \
-                           'P1_DOWN': 6, \
-                           'P1_LIGHT_PUNCH': 4, \
-                           'P1_MID_PUNCH': 2, \
-                           'P1_HEAVY_PUNCH': 3, \
-                           'P1_LIGHT_KICK': 8, \
-                           'P1_MID_KICK': 9, \
-                           'P1_HEAVY_KICK': 10, \
-                           'P1_PAUSE': 23, \
-
-                           'P2_LEFT': pygame.K_LEFT, \
-                           'P2_UP': pygame.K_UP, \
-                           'P2_DOWN': pygame.K_DOWN, \
-                           'P2_LIGHT_PUNCH': pygame.K_q, \
-                           'P2_MID_PUNCH': pygame.K_w, \
-                           'P2_HEAVY_PUNCH': pygame.K_e, \
-                           'P2_LIGHT_KICK': pygame.K_a, \
-                           'P2_MID_KICK': pygame.K_s, \
-                           'P2_HEAVY_KICK': pygame.K_d, \
-                           'P2_PAUSE': pygame.K_ESCAPE \
-                           }
+                self.game_pads.append(pg.joystick.Joystick(i))
+                self.game_pads[i].init()  # must be initialized in the list
+                print('.Game pad detected: {} assigned to player {}'.format(self.game_pads[i].get_name(), (i + 1)))
+                print(self.game_pads[i].get_numbuttons(), 'buttons detected on joystick', i)
+                i = i + 1
         else:
-            self.map = self.default
+            print("No joystick detected. SHAME ON YOU")
+
+
 
     def __init__(self):
         self.set_gamepads()
+'''
+def get_down(self, key):
+    Use the pygame function to get input
+    if self.bmap.game_pad_present:
+        return self.game_pads[0].get_button(7)
+    else:
+        return pg.key.get_pressed()[self.map[key]]
+
+'''
 
 
-    def get_down(self, key):
-        '''Use the pygame function to get input'''
-        if self.game_pad_present:
-            return self.game_pads[0].get_button(7)
-        else:
-            return pygame.key.get_pressed()[self.map[key]]
 
-    def change_mapping(self, key, pygame_constant):
-        '''Changes the mapping if it is defined'''
-        try:
-            self.map[key] # this will throw KeyError before next line runs
-                          # key doesn't exist
-            self.map[key] = pygame_constant
-            return True
-        except KeyError:
-            return False
+'''Handles the key/joystick events continuously'''
 
 
 class Handler:
-    class __Handler: # this bit makes it a singleton
+    class __Handler:  # this bit makes it a singleton
         bmap = ButtonMap()
-        keys = {}
+        keys = bmap.input
+
         def __init__(self):
 
-            # store the value of each predefined input button
-            self.keys={'P1_RIGHT': False,    \
-                        'P1_LEFT': False,    \
-                        'P1_UP'  : False,    \
-                        'P1_DOWN': False,    \
-                 'P1_LIGHT_PUNCH': False,    \
-                  'P1_MID_PUNCH' : False,    \
-                 'P1_HEAVY_PUNCH': False,    \
-                  'P1_LIGHT_KICK': False,    \
-                    'P1_MID_KICK': False,    \
-                  'P1_HEAVY_KICK': False,    \
-                       'P1_PAUSE': False, \
- \
-                       'P2_RIGHT': False, \
-                       'P2_LEFT': False, \
-                       'P2_UP': False, \
-                       'P2_DOWN': False, \
-                       'P2_LIGHT_PUNCH': False, \
-                       'P2_MID_PUNCH': False, \
-                       'P2_HEAVY_PUNCH': False, \
-                       'P2_LIGHT_KICK': False, \
-                       'P2_MID_KICK': False, \
-                       'P2_HEAVY_KICK': False, \
-                       'P2_PAUSE': False, \
-                       }
+            self.keys = {'P1_RIGHT': False,
+                         'P1_LEFT': False,
+                         'P1_UP': False,
+                         'P1_DOWN': False,
+                         'P1_LIGHT_PUNCH': False,
+                         'P1_MID_PUNCH': False,
+                         'P1_HEAVY_PUNCH': False,
+                         'P1_LIGHT_KICK': False,
+                         'P1_MID_KICK': False,
+                         'P1_HEAVY_KICK': False,
+                         'P1_PAUSE': False,
+
+                         'P2_RIGHT': False,
+                         'P2_LEFT': False,
+                         'P2_UP': False,
+                         'P2_DOWN': False,
+                         'P2_LIGHT_PUNCH': False,
+                         'P2_MID_PUNCH': False,
+                         'P2_HEAVY_PUNCH': False,
+                         'P2_LIGHT_KICK': False,
+                         'P2_MID_KICK': False,
+                         'P2_HEAVY_KICK': False,
+                         'P2_PAUSE': False,
+                         }
 
         def __str__(self):
             return repr(self)
 
+        # Checks an individual button to see if it is being pressed. Returns boolean value
+        def get_down(self, button):
+            if self.bmap.game_pad_present:
+                print('Found gamepad')
+                return self.bmap.get_down_gamepad(self.bmap.joystick_map_1_player[button])
+            else:
+                return self.bmap.get_down_keyboard(self.bmap.keyboard_map[button])
+
+        # Check each button to see if it is being pressed. Sets button map values to true for buttons pushed
         def handle_input(self):
-            '''The big input switch
-            statement that quantifies all
-            the input'''
-            for key in self.bmap.map.keys():
-                self.keys[key] = self.bmap.get_down(key)
+            for button in self.keys:
+                self.keys[button] = self.get_down(button)
 
-        def get_down(self, key):
-            return self.keys[key]
-
+        # Returns just the keys that are active as a list.
         def get_active_keys(self):
             temp = []
-            for key in self.keys.keys():
-                if self.keys[key]:
-                    temp.append(key)
+            for button in self.keys.keys():
+                if self.keys[button]:
+                    temp.append(button)
             return temp
 
     iHandler = None
 
     def __init__(self):
-        '''Singleton to return
-        input handler so every module
-        has the same instance'''
+        # Singleton to return input handler so every module has the same instance'''
         if not Handler.iHandler:
             Handler.iHandler = Handler.__Handler()
 
@@ -161,7 +194,3 @@ class Handler:
 
     def __getattr__(self, item):
         return getattr(Handler.iHandler, item)
-
-
-
-
