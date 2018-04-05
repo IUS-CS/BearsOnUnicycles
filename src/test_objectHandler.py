54c39f2ba95c86cd85c951549bd558e787ce08db
igillenh2o <igillenh2o@gmail.com>
2018-04-05 15:18:00 -0400

    Switched something else back
    switched back to old form

--- src/test_objectHandler.py
+++ src/test_objectHandler.py
@@ -1,12 +1,12 @@
+
 from unittest import TestCase
-from old import ObjectHandler
-'''
+from ObjectHandler import ObjectHandler
+
 class TestObjectHandler(TestCase):
     def test_setupScreen(self):
-        example = ObjectHandler.ObjectHandler()
+        example = ObjectHandler()
         self.assertTrue(example.setupScreen())
 
     def test_run(self):
-        example = ObjectHandler.ObjectHandler()
+        example = ObjectHandler()
         self.assertTrue(example.run())
-'''
